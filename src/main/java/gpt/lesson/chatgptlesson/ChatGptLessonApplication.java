8336ff871e4929fc07da8ec0c7c467125e4f4874
dquoupb <cmk5378@naver.com>
2026-05-14 20:38:30 +0900

    project init

---
+++ src/main/java/gpt/lesson/chatgptlesson/ChatGptLessonApplication.java
@@ -0,0 +1,13 @@
+package gpt.lesson.chatgptlesson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptLessonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gpt/lesson/chatgptlesson/ChatGptLessonApplicationTests.java
@@ -0,0 +1,13 @@
+package gpt.lesson.chatgptlesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptLessonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
